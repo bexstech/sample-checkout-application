e49f1144c9f31458d5c25c08def16a264b7dc7f8
Renan Maciel <renan.lima@bexsbanco.com.br>
2019-05-22 11:58:06 -0300

    Tela de seleção de plano

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = require("./config/express")();
 
-app.listen(3000, function(){
-    console.log("Servidor rodandos");    
+app.listen(3030, function(){
+    console.log("Listen on por: 3030");
 });
