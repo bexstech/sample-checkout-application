d9ec20f517e56327d28d92c8703c1886c3aa4e68
hakd26 <henrique.diomede@bexsbanco.com.br>
2019-10-24 13:50:47 -0300

    teste

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require ('custom-env').env();
 var app = require("./config/express")();
 
-app.listen(80, function(){
-    console.log("Listen on port: 80");
+app.listen(9000, function(){
+    console.log("Listen on port: 9000");
 });
