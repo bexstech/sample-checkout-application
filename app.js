6aaa1722009f8929712dcc644416d7dea37986df
hakd26 <henrique.diomede@bexsbanco.com.br>
2019-10-24 13:57:01 -0300

    dynamic port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require ('custom-env').env();
 var app = require("./config/express")();
 
-app.listen(9000, function(){
+app.listen(process.env.PORT, function(){
     console.log("Listen on port: 9000");
 });
